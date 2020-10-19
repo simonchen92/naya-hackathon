92127a380d0e0025b5a779acc4f00a6da7bb6784
simonchen92 <simon.k.chen92@gmail.com>
2020-10-19 10:07:47 -0400

    Updates

---
+++ precache-manifest.c74dd170b16704c3067a085bc4961702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35ecdba7b89b345ad450fc8ddfbeb7f",
+    "url": "/naya-hackathon/index.html"
+  },
+  {
+    "revision": "868535f02067abdbd9bd",
+    "url": "/naya-hackathon/static/css/2.52f2db83.chunk.css"
+  },
+  {
+    "revision": "ab04c49b299486d726e6",
+    "url": "/naya-hackathon/static/css/main.fec6b590.chunk.css"
+  },
+  {
+    "revision": "868535f02067abdbd9bd",
+    "url": "/naya-hackathon/static/js/2.560d515d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/naya-hackathon/static/js/2.560d515d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab04c49b299486d726e6",
+    "url": "/naya-hackathon/static/js/main.9a8f48c6.chunk.js"
+  },
+  {
+    "revision": "3e0f38eef4299908389a",
+    "url": "/naya-hackathon/static/js/runtime-main.28838b8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85847e4f1f6da95b1ce7d4cb67e215b7.js"
+  "/naya-hackathon/precache-manifest.c74dd170b16704c3067a085bc4961702.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/naya-hackathon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
